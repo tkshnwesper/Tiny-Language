44826cd082a0f80b78c65ef7688c4ef26eda5939
Saurabh M. Machave <saurabh.macheve@thetrainline.com>
2018-05-01 02:37:15 +0530

    completed 15 test milestone in regex parser

--- main/regex.c
+++ main/regex.c
@@ -1,11 +1,123 @@
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "regex.h"
 
-char *match(char *string, char *pattern) {
-  if (string == NULL) return NULL;
-  if (pattern == NULL) return "";
-  if (strlen(string) == 0) {
-    return "";
+typedef struct properties {
+  int paren_start;
+  int paren_end;
+  int escape;
+  int dollar;
+  int caret;
+} properties, *PROPERTIES;
+
+char *append_to_result(char *result, char appendee) {
+  if (result != NULL) {
+    int len = strlen(result);
+    char *new_result = (char *) malloc(len + 2);
+    new_result[len + 1] = '\0';
+    strcpy(new_result, result);
+    new_result[len] = appendee;
+    free(result);
+    return new_result;
   }
-  return pattern;
+  char *new_result = (char *) malloc(2);
+  new_result[1] = '\0';
+  new_result[0] = appendee;
+  return new_result;
+}
+
+void escape_consequences(int *escape) {
+  if (*escape) {
+    *escape = 0;
+  }
+}
+
+int append_on_match(char **result, char *string, char letter, int i, PROPERTIES props, int count) {
+  if (string[i - props->paren_start - props->paren_end - count] == letter) {
+    *result = append_to_result(*result, letter);
+    return 1;
+  }
+  return 0;
+}
+
+void loop_for_your_own_good(char *string, char *pattern, char **result, PROPERTIES props) {
+  int escaped_character_count = 0;
+  for (int i = 0; i < strlen(pattern); i++) {
+    int no_hope_left = 0;
+    switch (pattern[i]) {
+      case '\\':
+        props->escape = 1;
+        escaped_character_count++;
+        break;
+      case '(': {
+        if (props->escape) {
+          escape_consequences(&props->escape);
+          append_on_match(result, string, pattern[i], i, props, escaped_character_count);
+        } else {
+          props->paren_start = 1;
+        }
+        break;
+      }
+      case ')': {
+        if (props->escape) {
+          escape_consequences(&props->escape);
+          append_on_match(result, string, pattern[i], i, props, escaped_character_count);
+        } else {
+          props->paren_end = 1;
+        }
+        break;
+      }
+      default: {
+        if (!append_on_match(result, string, pattern[i], i, props, escaped_character_count)) {
+          no_hope_left = 1;
+          if (*result != NULL) {
+            free(*result);
+            *result = NULL;
+          }
+          break;
+        }
+      }
+    }
+    if (no_hope_left) break;
+  }
+}
+
+char *match(char *string, char *pattern) {
+  if (string == NULL || strlen(string) == 0 || pattern == NULL) return NULL;
+  char *result = NULL;
+  properties props;
+  props.paren_start = 0;
+  props.paren_end = 0;
+  props.escape = 0;
+  props.dollar = pattern[strlen(pattern) - 1] == '$';
+  props.caret = pattern[0] == '^';
+  if (props.dollar) {
+    char *dup = strdup(pattern);
+    dup[strlen(dup) - 1] = '\0';
+    pattern = dup;
+  }
+  if (props.caret) {
+    pattern = pattern + 1;
+    loop_for_your_own_good(string, pattern, &result, &props);
+  } else {
+    char *partial = string;
+    do {
+      loop_for_your_own_good(partial, pattern, &result, &props);
+      if (result != NULL) break;
+    } while (*(++partial) != '\0');
+  }
+  if (result != NULL && props.dollar) {
+    char *partial = string;
+    int verified = 0;
+    while(partial != '\0' && (partial = strstr(partial, result)) != NULL) {
+      if (partial[strlen(result)] == '\0') {
+        verified = 1;
+        break;
+      }
+      ++partial;
+    }
+    return verified ? result : NULL;
+  }
+  return result;
 }
--- test/regex_test.c
+++ test/regex_test.c
@@ -5,8 +5,8 @@ void should_return_null_when_source_is_null() {
   TEST_ASSERT_NULL(match(NULL, ""));
 }
 
-void should_return_empty_string_when_source_string_is_empty() {
-  TEST_ASSERT_EQUAL_STRING("", match("", "nihao"));
+void should_return_null_when_source_string_is_empty() {
+  TEST_ASSERT_NULL(match("", "nihao"));
 }
 
 void should_return_string_itself_when_pattern_is_equal_to_source() {
@@ -17,16 +17,66 @@ void should_return_partial_match_when_pattern_is_substring_of_source() {
   TEST_ASSERT_EQUAL_STRING("hel", match("hello", "hel"));
 }
 
-void should_return_empty_string_when_pattern_is_null() {
-  TEST_ASSERT_EQUAL_STRING("", match("hello", NULL));
+void should_return_null_when_pattern_is_null() {
+  TEST_ASSERT_NULL(match("hello", NULL));
+}
+
+void should_return_content_within_parentheses_on_match() {
+  TEST_ASSERT_EQUAL_STRING("hel", match("hello", "(hel)"));
+}
+
+void should_return_null_when_source_and_pattern_do_not_match() {
+  TEST_ASSERT_NULL(match("meow", "hello"));
+}
+
+void should_return_null_when_source_and_pattern_do_not_match_with_parentheses() {
+  TEST_ASSERT_NULL(match("hello", "(heq)"));
+}
+
+void should_return_null_when_match_is_found_in_between_parentheses_but_not_outside() {
+  TEST_ASSERT_NULL(match("jello", "h(ell)o"));
+}
+
+void should_return_null_when_there_is_mismatch_in_string_after_parentheses() {
+  TEST_ASSERT_NULL(match("jello", "je(ll)p"));
+}
+
+void should_not_read_a_paren_that_is_part_of_a_string_as_open_paren() {
+  TEST_ASSERT_EQUAL_STRING("(meow", match("(meow", "\\(meow"));
+}
+
+void should_compare_from_start_when_caret_symbol_is_at_beginning() {
+  TEST_ASSERT_EQUAL_STRING("me", match("meow", "^me"));
+}
+
+void should_not_match_if_caret_symbol_is_at_start_but_pattern_does_not_match_from_beginning() {
+  TEST_ASSERT_NULL(match("meow", "^ow"));
+}
+
+void should_compare_from_end_when_dollar_symbol_is_at_end() {
+  TEST_ASSERT_EQUAL_STRING("ow", match("meow", "ow$"));
+}
+
+void should_not_match_if_dollar_symbol_is_at_end_but_pattern_does_not_match_from_end() {
+  TEST_ASSERT_NULL(match("meow", "me$"));
 }
 
 int main() {
   UNITY_BEGIN();
-  RUN_TEST(should_return_empty_string_when_source_string_is_empty);
   RUN_TEST(should_return_null_when_source_is_null);
+  RUN_TEST(should_return_null_when_source_string_is_empty);
   RUN_TEST(should_return_string_itself_when_pattern_is_equal_to_source);
   RUN_TEST(should_return_partial_match_when_pattern_is_substring_of_source);
-  RUN_TEST(should_return_empty_string_when_pattern_is_null);
+  RUN_TEST(should_return_null_when_pattern_is_null);
+  RUN_TEST(should_return_content_within_parentheses_on_match);
+  RUN_TEST(should_return_null_when_source_and_pattern_do_not_match);
+  RUN_TEST(should_return_null_when_source_and_pattern_do_not_match_with_parentheses);
+  RUN_TEST(should_return_null_when_match_is_found_in_between_parentheses_but_not_outside);
+  RUN_TEST(should_return_null_when_there_is_mismatch_in_string_after_parentheses);
+  RUN_TEST(should_not_read_a_paren_that_is_part_of_a_string_as_open_paren);
+  RUN_TEST(should_compare_from_start_when_caret_symbol_is_at_beginning);
+  RUN_TEST(should_not_match_if_caret_symbol_is_at_start_but_pattern_does_not_match_from_beginning);
+  RUN_TEST(should_compare_from_end_when_dollar_symbol_is_at_end);
+  RUN_TEST(should_not_match_if_dollar_symbol_is_at_end_but_pattern_does_not_match_from_end);
   return UNITY_END();
 }
